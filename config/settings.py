fdcbb34a6ed1548849b593bf7e55ca08604e056e
Mate <ekwadorow@gmail.com>
2026-09-09 13:32:43 +0200

    Dodanie Celery i Redis

--- config/settings.py
+++ config/settings.py
@@ -135,3 +135,14 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+CELERY_BROKER_URL = os.environ.get(
+    "CELERY_BROKER_URL",
+    "redis://localhost:6379/0",
+)
+
+CELERY_RESULT_BACKEND = os.environ.get(
+    "CELERY_RESULT_BACKEND",
+    "redis://localhost:6379/0",
+)
--- core/tests.py
+++ core/tests.py
@@ -4,3 +4,16 @@ from django.test import SimpleTestCase
 class BasicTest(SimpleTestCase):
     def test_example(self):
         self.assertEqual(2 + 2, 4)
+
+
+from django.test import SimpleTestCase
+
+from .tasks import add
+
+
+class BasicTest(SimpleTestCase):
+    def test_example(self):
+        self.assertEqual(2 + 2, 4)
+
+    def test_celery_add_task(self):
+        self.assertEqual(add.run(2, 3), 5)
