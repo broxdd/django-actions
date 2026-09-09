72e889d523e6b05f872d69e9609af3c1ea0bf8bf
Mate <ekwadorow@gmail.com>
2026-09-09 11:36:08 +0200

    Konfiguracja GitHub Secret

--- config/settings.py
+++ config/settings.py
@@ -1,3 +1,5 @@
+import os
+
 """
 Django settings for config project.
 
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^^q&0iziq49)%5^6_qny&eg@t0r8f=xd%nu*xo8azvf19pe(9g'
+SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
